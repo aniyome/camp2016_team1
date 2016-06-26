49b2cfb99a4fb144e20bba502e708278a252b453
Kazuki Kumazawa <kumazawa730@gmail.com>
2016-06-27 00:59:46 +0900

    HPバーを追加

--- Unity/CampGame/CampGame/Assets/Scripts/CharacterStatus.cs
+++ Unity/CampGame/CampGame/Assets/Scripts/CharacterStatus.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CharacterStatus : MonoBehaviour {
 
@@ -15,9 +16,19 @@ public class CharacterStatus : MonoBehaviour {
 	// MP
 	private float MP;
 
+	// HPバー
+	private Slider slider;
+
+	// HPバーの値
+	private float sliderValue = 1;
+
 	// Use this for initialization
 	void Start () {
+		// MaxHPを現在のHPに設定
 		HP = MaxHP;
+
+		// HPバーのオブジェクト取得
+		slider = GetComponentInChildren<Slider>();
 	}
 	
 	// Update is called once per frame
@@ -25,9 +36,17 @@ public class CharacterStatus : MonoBehaviour {
 
 	}
 
-	// ダメージ加算処理
+	// ダメージ計算処理
 	public void Damage (float damage) {
+		// HP減算処理
 		HP = HP - damage;
+
+		// HPバーの値減少処理
+		slider.value = HP / MaxHP;
+
+		Debug.Log ("残りゲージは" + slider.value);
+		Debug.Log ("残りHPは" + HP);
+
 		// HPが無くなった場合の処理
 		if (HP <= 0) {
 			Destroy(gameObject);
