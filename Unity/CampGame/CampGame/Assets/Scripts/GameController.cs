8a88da3e174b7ebe98f1a3e4a03094ae777120b6
k.kumazawa <k.kumazawa@aveliouser-no-Mac-mini.local>
2016-06-30 22:50:43 +0900

    制限時間表示処理を実装

--- Unity/CampGame/CampGame/Assets/Scripts/GameController.cs
+++ Unity/CampGame/CampGame/Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
@@ -9,8 +10,14 @@ public class GameController : MonoBehaviour {
 	// プレイヤーHPラベルオブジェクト
 	public UnityEngine.UI.Text PlayerHpLabel;
 
+	// 制限時間ラベルオブジェクト
+	public UnityEngine.UI.Text TimerLabel;
+
 	// プレイヤーオブジェクト
 	public GameObject Player;
+
+	// 制限時間
+	public float TimeLimit;
 
 	// スコア
 	private float Score;
@@ -18,21 +25,33 @@ public class GameController : MonoBehaviour {
 	// プレイヤーのHP
 	private float PlayerHP;
 
+	// 残り時間
+	private float RemainingTime;
+
 	// Use this for initialization
 	void Awake () {
 		// 初期スコアセット
 		Score = 0;
+		ScoreLabel.text = "Score:" + Score.ToString();
 		// プレイヤーの初期HP取得
 		PlayerHP = Player.GetComponent<PlayerStatus>().MaxHP;
+		PlayerHpLabel.text = "HP:" + PlayerHP.ToString ();
+		// 残り時間セット
+		RemainingTime = TimeLimit;
+		TimerLabel.text = "Time:" + ((int)RemainingTime).ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// スコアを表示
-		ScoreLabel.text = "SCORE:" + Score.ToString();
+		ScoreLabel.text = "Score:" + Score.ToString();
 
 		// プレイヤーのHPを表示
-		PlayerHpLabel.text = "HP：" +  Player.GetComponent<PlayerStatus>().HP;
+		PlayerHpLabel.text = "HP:" +  Player.GetComponent<PlayerStatus>().HP;
+
+		// 制限時間を表示
+		RemainingTime -= Time.deltaTime;
+		TimerLabel.text = "Time:" + ((int)RemainingTime).ToString();
 	}
 
 	// スコア加算処理
