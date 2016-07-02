3d04deb2e57fe2fd8e434fcf192674aea1c12996
k.kumazawa <k.kumazawa@aveliouser-no-Mac-mini.local>
2016-07-02 10:14:49 +0900

    ハートマークのHP表示のAssetを追加

---
+++ Unity/CampGame/CampGame/Assets/K2 Games/Icon Based Health Bars/Scripts/Editor/SplitIconProgressBarEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(SplitIconProgressBar))]
+public class SplitIconProgressBarEditor : IconProgressBarEditor
+{
+    SerializedProperty splitSpritePrefab;
+
+    new void OnEnable()
+    {
+        splitSpritePrefab = serializedObject.FindProperty("splitSpritePrefab");
+
+        base.OnEnable();
+    }
+
+    protected override void ShowMainImage()
+    {
+        EditorGUILayout.PropertyField(splitSpritePrefab, new GUIContent("Split Sprite Prefab"));
+    }
+}
