164cd253ea00b99d0f7b6a6cbf752b8db4c07856
j-sakuma <j_sakuma@avelio.co.jp>
2016-07-06 10:23:53 +0900

    アイテム追加(弾数max上昇)

---
+++ Unity/CampGame/CampGame/Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+  public int maxBulletCount = 40;
+
+  void Start () {
+  }
+
+  void Update () {
+  }
+
+  // 接触判定(接触オブジェクト)
+  void OnTriggerEnter (Collider other) {
+    if (other.tag == "Player") {
+      Destroy(gameObject);
+      other.GetComponent<PlayerStatus>().maxBulletCount += maxBulletCount;
+    }
+  }
+}
