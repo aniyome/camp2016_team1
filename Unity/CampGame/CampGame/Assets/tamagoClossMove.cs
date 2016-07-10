978df1fb358a9a3335713727f008542839b90ca8
AvelioDeveloper <aveliodeveloper@GreenRabbit.local>
2016-07-10 09:26:36 +0900

    tekihaichi

---
+++ Unity/CampGame/CampGame/Assets/tamagoClossMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tamagoClossMove : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = 5 * Mathf.Sin(Time.time);
+		var z = 5 * Mathf.Cos(Time.time);
+		transform.position = new Vector3(x, 0, z);
+
+	}
+}
