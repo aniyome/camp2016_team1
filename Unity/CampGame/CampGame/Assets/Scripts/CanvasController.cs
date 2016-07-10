f330156cff1ea2d0431894f83df35105b573c6ef
j-sakuma <j_sakuma@avelio.co.jp>
2016-07-10 13:42:11 +0900

    fix

--- Unity/CampGame/CampGame/Assets/Scripts/CanvasController.cs
+++ Unity/CampGame/CampGame/Assets/Scripts/CanvasController.cs
@@ -61,9 +61,9 @@ public class CanvasController : MonoBehaviour {
 		TimerLabel.text = "Time:" + ((int)TimeLimit).ToString();
 		// 一定時間後にMissionTextを消去
 		// Destroy(MissionText, 3.0f);
-		Destroy(MissionText, 0.1f);
+		Destroy(MissionText, 0.5f);
 		// Destroy(transform.FindChild("BlackImage").gameObject, 3.0f);
-		Destroy(transform.FindChild("BlackImage").gameObject, 0.1f);
+		Destroy(transform.FindChild("BlackImage").gameObject, 0.5f);
 		// 弾数を表示
 		BulletSlider = GameObject.Find("BulletSlider").GetComponent<Slider>();
 		BulletSlider.maxValue = Player.GetComponent<PlayerStatus>().maxBulletCount;
